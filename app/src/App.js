8d5926dea683ee637921e53642fbdfc7db6e31ec
Arek Kuźma <kuzmaarek5@gmail.com>
2024-02-20 04:10:34 +0100

    Improve css style button

--- app/src/App.js
+++ app/src/App.js
@@ -39,7 +39,6 @@ const App = () => {
   };
   const handleOutsideClick = (event) => {
     console.log(event);
-    // Sprawdź, czy kliknięcie nastąpiło poza obszarem AnimatePresence
     if (selectedId && !event.target.closest(".animate-presence")) {
       setSelectedId(null);
     }
@@ -48,58 +47,25 @@ const App = () => {
   return (
     <div onClick={handleOutsideClick} className="flex gap-[12px]">
       <div>
-        <div className="text-[#1c1c1c] font-[14px] font-[600] ">
-          Select file
-        </div>
-        <div className="bg-[#e3f5ff] h-[50px] rounded-[16px] custom-box-shadow">
-          <input type="file" ref={fileInputRef} accept=".csv" />
+        <div className="text-[#1c1c1c] font-[14px] font-[600]">Select file</div>
+        <div className="bg-[#e3f5ff]  rounded-[16px] custom-box-shadow p-8">
+          <input
+            type="file"
+            className="mb-4"
+            ref={fileInputRef}
+            accept=".csv"
+          />
           <button
-            className="relative uppercase spacing tracking-widest font-[400] text-base px-[10px] py-[20px] duration-500 
-            w-[200px]
-            hover:bg-[#A1E3CB] hover:text-[#fff] hover:tracking-[0.25em] 
-            before:content-[''] 
-            before:bg-[green] 
-            before:absolute
-            before:inset-[2px]
-            group"
+            className="relative uppercase spacing tracking-widest font-[400] text-base py-[10px] duration-500 w-[150px] rounded-[16px] border-[2px] border-[#A1E3CB] text-[#1c1c1c] group
+            hover:bg-[#A1E3CB] hover:tracking-[0.25em] before:content-[''] before:absolute before:inset-[2px]"
             onClick={handleCsvSubmission}
           >
-            <span className="relative z-10">Submit</span>
+            <span className="relative z-10 flex justify-center">Submit</span>
             <i
-              className="
-            group-hover:before:w-[6px] group-hover:before:left-[calc(50%)] 
-            group-hover:before:delay-[0.5s]
-            box-shadow-button
-            before:absolute
-            before:content-[''] 
-            before:w-[10px] 
-            before:h-[6px] 
-            before:bg-[white] 
-            before:border-[2px] 
-            before:border-[#A1E3CB] 
-            before:top-[-3.5px] 
-            before:left-[80%] 
-            before:translate-x-[-50%] 
-            before:duration-500
-            before:delay-[0.5s]
-            
-            group-hover:after:w-[6px] group-hover:after:left-[calc(50%)] 
-            group-hover:after:delay-[0.5s]
-           
-            after:absolute
-            after:content-[''] 
-            after:w-[10px] 
-            after:h-[6px] 
-            after:bg-[white] 
-            after:border-[2px] 
-            after:border-[#A1E3CB] 
-            after:bottom-[-3.5px] 
-            after:left-[20%] 
-            after:translate-x-[-50%] 
-            after:duration-500
-            after:delay-[0.5s]
-            
-            "
+              className="box-shadow-button group-hover:before:w-[8px] group-hover:before:left-[calc(50%)]  group-hover:before:delay-[0.5s]
+              before:content-[''] before:absolute  before:w-[10px] before:h-[6px] before:bg-[white] before:border-[2px] before:border-[#A1E3CB] before:top-[-3.5px] before:left-[80%] before:translate-x-[-50%] before:duration-500 before:delay-[0.5s]
+              group-hover:after:w-[8px]  group-hover:after:left-[calc(50%)]  group-hover:after:delay-[0.5s]
+              after:content-[''] after:absolute after:w-[10px] after:h-[6px] after:bg-[white] after:border-[2px] after:border-[#A1E3CB] after:bottom-[-3.5px] after:left-[20%] after:translate-x-[-50%] after:duration-500 after:delay-[0.5s]"
             ></i>
           </button>
         </div>
